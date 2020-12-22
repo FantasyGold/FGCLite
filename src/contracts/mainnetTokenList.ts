6ef18b20e5bd3824de15817668be7415070413d6
NicoDFS <admin@majorleaguedfs.com>
2020-12-22 15:53:53 -0500

    Add MAZER Token

--- src/contracts/mainnetTokenList.ts
+++ src/contracts/mainnetTokenList.ts
@@ -3,7 +3,8 @@ import FGCToken from '../models/FGCToken';
 const mainnetTokenList: FGCToken[] = [
   new FGCToken('Nico Token', 'NICO', 8, '77b9c2de95908905b36cdba5cd1958053b2437bb'),
   new FGCToken('Fantasy Token', 'FAN', 8, '7564b6686fb62d1de08de677abff9da0a516a692'),
-  new FGCToken('Draft Dollars', 'DD', 8, 'fe59cbc1704e89a698571413a81f0de9d8f00c69')
+  new FGCToken('Draft Dollars', 'DD', 8, 'fe59cbc1704e89a698571413a81f0de9d8f00c69'),
+  new FGCToken('Mazer Gaming Token', 'MAZER', 8, '27a9078e5289b5e9776e96f0a40ce3f103b5711b')
 ];
 
 export default mainnetTokenList;
